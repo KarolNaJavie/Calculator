2ddcb54a2c3dd5fd7cbbeb832069b22782dafed8
KarolNaJavie <dsztacheta@gmail.com>
2025-11-23 20:55:31 +0100

    Revert "dodanie podziekowania"
    This reverts commit 34351612592fd7555a6942ffc5ebbdf179a6846b.

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,6 @@ void main() {
         if (choice == 1) {
             //cos tutaj
             System.out.println(Calculable.adding(a, b));
-            System.out.println("dziekuje za skorzystanie z uslug");
         } else if (choice == 2) {
             System.out.println(Calculable.subtracting(a, b));
         } else if (choice == 3) {
